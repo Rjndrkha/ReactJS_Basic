22e05523559960e8097598644209fc8d041ff377
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-05-16 20:31:14 +0700

    Product Info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  //const userLogged = "Rajendra Rakha";
+  //const userLoggedJSX = <strong>Rajendra Rakha</strong>;
+
+  const category = "RUNNING SHOES";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="ParentBox">
+
+      <div class="Foto">
+        <img alt="sepatu" src="shoes.jpg" />
+      </div>
+      <ProductInfo/>
+
     </div>
   );
 }
 
+function ProductInfo() {
+
+  const category = "RUNNING SHOES";
+  return (
+    <div >
+      <div class="Deskripsi">
+        <p class="Cate">{category}</p>
+        <h1 class="Title">Pinky Swagg</h1>
+        <p class="Price">IDR 3.000.000</p>
+        <p class="Info">
+          Sepatu Off White Limited Edition
+        </p>
+
+      </div>
+    </div>
+  );
+}
 export default App;
